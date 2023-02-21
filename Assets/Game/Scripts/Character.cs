739d947d9936646eabf04b6dfeae5e7efc429404
tiger <thisisstreamliner@gmail.com>
2023-02-21 19:14:09 +0200

    Fix Vertical Movement

--- Assets/Game/Scripts/Character.cs
+++ Assets/Game/Scripts/Character.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float MoveSpeed = 5f;
+    public float Gravity = -9.8f;
 
     private CharacterController _characterController;
     private Vector3 _movementVelocity;
     private PlayerInput _playerInput;
+    private float _verticalVelocity;
 
     // Update is called once per frame
     private void Awake()
@@ -23,11 +25,25 @@ public class Character : MonoBehaviour
         _movementVelocity.Normalize();
         _movementVelocity = Quaternion.Euler(0, -45f, 0) * _movementVelocity;
         _movementVelocity *= MoveSpeed * Time.deltaTime;
+
+        if (_movementVelocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_movementVelocity);
+        }
     }
 
     private void FixedUpdate()
     {
         CalculatePlayerMovement();
+        if (!_characterController.isGrounded)
+        {
+            _verticalVelocity = Gravity;
+        }
+        else
+        {
+            _verticalVelocity = Gravity * 0.3f;
+        }
+        _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
         _characterController.Move(_movementVelocity);
     }
 }
